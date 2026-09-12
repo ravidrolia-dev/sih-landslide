b71101784cacda883cf5dff6b7954d868b6cf622
ravidrolia-dev <ravi06drolia@gmail.com>
2026-09-12 18:13:25 +0530

    Set Satellite Imagery (Esri) as default map view

--- frontend/src/RiskMap.jsx
+++ frontend/src/RiskMap.jsx
@@ -129,7 +129,7 @@ const RiskMap = ({ selectedLocation, onLocationSelect, heatmapData, routeData, f
         <MapViewUpdater targetLoc={selectedLocation} routeData={routeData} />
         
         <LayersControl position="topright">
-          <BaseLayer checked name="OpenTopoMap (Topographic GIS)">
+          <BaseLayer name="OpenTopoMap (Topographic GIS)">
             <TileLayer
               attribution='&copy; <a href="https://opentopomap.org">OpenTopoMap</a>'
               url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
@@ -137,7 +137,7 @@ const RiskMap = ({ selectedLocation, onLocationSelect, heatmapData, routeData, f
             />
           </BaseLayer>
 
-          <BaseLayer name="Satellite Imagery (Esri)">
+          <BaseLayer checked name="Satellite Imagery (Esri)">
             <TileLayer
               attribution='&copy; <a href="https://www.esri.com/">Esri</a>'
               url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
